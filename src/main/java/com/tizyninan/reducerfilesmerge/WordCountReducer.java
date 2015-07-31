47b3492c71a486c95347934c1f9b3acbec9472ca
tizy <tizy@qburst.com>
2015-07-31 11:43:59 +0530

    Initial commit

---
+++ src/main/java/com/tizyninan/reducerfilesmerge/WordCountReducer.java
@@ -0,0 +1,7 @@
+package com.tizyninan.reducerfilesmerge;
+
+/**
+ * Created by qbuser on 31/7/15.
+ */
+public class WordCountReducer {
+}
---
+++ src/main/java/com/tizyninan/reducerfilesmerge/WordcountDriver.java
@@ -0,0 +1,7 @@
+package com.tizyninan.reducerfilesmerge;
+
+/**
+ * Created by tizy on 31/7/15.
+ */
+public class WordcountDriver {
+}
