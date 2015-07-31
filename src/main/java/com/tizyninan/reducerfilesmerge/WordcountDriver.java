ce7bdc7608d48e81fec1067cb9782c7f20c3ae05
tizy <tizy@qburst.com>
2015-07-31 15:12:57 +0530

    Added code to merge reducer output files to a single file in HDFS

--- src/main/java/com/tizyninan/reducerfilesmerge/WordcountDriver.java
+++ src/main/java/com/tizyninan/reducerfilesmerge/WordcountDriver.java
@@ -1,7 +1,70 @@
 package com.tizyninan.reducerfilesmerge;
 
-/**
- * Created by tizy on 31/7/15.
- */
-public class WordcountDriver {
+import com.tizyninan.reducerfilesmerge.constants.Constants;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+
+public class WordcountDriver extends Configured implements Tool {
+
+    private static Configuration conf = new Configuration();
+
+    public static void main(String[] args) throws Exception {
+        String source = args[1];
+        String destination = args[3];
+        int result = ToolRunner.run(conf, new WordcountDriver(), args);
+        if(result == 0)
+            new WordcountDriver().mergeFilesInHDFS(source, destination);
+        System.exit(result);
+    }
+
+    private void mergeFilesInHDFS(String source, String destination) throws IOException {
+        FileSystem fs = FileSystem.get(conf);
+        Path sourcePath = new Path(source);
+        Path destPath = new Path(destination);
+        FileUtil.copyMerge(fs, sourcePath, fs, destPath, false, conf, null);
+    }
+
+
+    @Override
+    public int run(String[] strings) throws Exception {
+        Configuration conf = this.getConf();
+        Path inputDir = new Path(strings[0]);
+        Path outputDir = new Path(strings[1]);
+
+        Job job = Job.getInstance(conf, "SingleFileWrite");
+
+        job.setJarByClass(WordcountDriver.class);
+        job.setMapperClass(WordcountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, inputDir);
+        FileOutputFormat.setOutputPath(job, outputDir);
+
+        MultipleOutputs.addNamedOutput(job, Constants.REDUCER_FILE_NAME, TextOutputFormat.class, Text.class, IntWritable.class);
+
+        return (job.waitForCompletion(true)?0:1);
+    }
 }
--- src/main/java/com/tizyninan/reducerfilesmerge/WordcountMapper.java
+++ src/main/java/com/tizyninan/reducerfilesmerge/WordcountMapper.java
@@ -1,7 +1,26 @@
 package com.tizyninan.reducerfilesmerge;
 
-/**
- * Created by qbuser on 31/7/15.
- */
-public class WordcountMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class WordcountMapper extends Mapper<LongWritable,Text,Text,IntWritable> {
+
+    IntWritable one = new IntWritable(1);
+    Text outputKey = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while(tokenizer.hasMoreTokens()) {
+            outputKey.set(tokenizer.nextToken());
+            context.write(new Text(outputKey),one);
+        }
+    }
 }
